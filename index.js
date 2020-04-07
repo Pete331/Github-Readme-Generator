93a43486da9bafe04d3c79f177b43a9ceaf8b35a
Pete Brennan <pweda64@hotmail.com>
2020-04-07 14:53:44 +0800

    got a response from github

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-const questions = [
+const fs = require("fs");
+const api = require("./utils/api.js");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-];
-
-function writeToFile(fileName, data) {
+//inquirer function
+function askUser() {
+  return inquirer.prompt({
+    message: "Enter your GitHub username",
+    name: "username",
+  });
 }
 
-function init() {
+const questions = [];
 
+function writeToFile(fileName, data) {}
+
+async function init() {
+  try {
+    //storing answers from Inquirer as object variable
+    const user = await askUser();
+
+    //using Inquirer response to make queryURL
+    const queryURL = `https://api.github.com/users/${user.username}`;
+
+    //using stored QueryURLs to make API calls using Axios
+    const response = await axios.get(queryURL);
+
+    console.log(response.data);
+
+    // store required responses in object
+    const userInfo = {
+      user: user.username,
+      email: response.data.avatar_url.email,
+      profilePic: response.data.avatar_url,
+    };
+    console.log(userInfo);
+
+    // run the questions function ----- can run another await like (var something = await askQuestions();)
+    askQuestions(userInfo);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function askQuestions(userInfo){
+    console.log(userInfo);
 }
 
 init();
